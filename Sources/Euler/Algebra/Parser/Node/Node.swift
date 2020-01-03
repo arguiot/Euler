1b01df0244b31494de334643a02109fccac2abf3
Arthur Guiot <arguiot@gmail.com>
2020-01-02 21:15:17 -0500

    Fixed { get set } problem

--- Sources/Euler/Algebra/Parser/Node/Node.swift
+++ Sources/Euler/Algebra/Parser/Node/Node.swift
@@ -9,12 +9,12 @@ import Foundation
 /// An entity that helps build a tree when parsing math expressions
 public protocol Node: NSObject {
     /// This will be the string representation of the Node, it may change depending on the type of the Node
-    var content: String { get }
+    var content: String { get set }
     /// The name in camelCase of the Node.
     /// > Ex: `"ExpressionNode"` or `"OperatorNode"`
     var type: String { get }
     /// The Node's children in the expression tree
-    var children: [Node] { get }
+    var children: [Node] { get set }
     
     /// Evaluate the mathematical expression behind each nodes
     /// - Parameter params: The object that contains all the variable
