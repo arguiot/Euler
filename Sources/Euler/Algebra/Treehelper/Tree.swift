e6705afd79f18f16fd6d529d418b55ce9fd03760
Arthur Guiot <arguiot@gmail.com>
2020-01-02 19:37:45 -0500

    Fixes Travis build

---
+++ Sources/Euler/Algebra/Treehelper/Tree.swift
@@ -0,0 +1,21 @@
+//
+//  Tree.swift
+//  
+//
+//  Created by Arthur Guiot on 2020-01-02.
+//
+
+import Foundation
+
+/// A Tree object containing the Tree of a parsed expression. It helps to do various operations on the expression
+class Tree: NSObject {
+    var expression: ExpressionNode
+    init(_ expr: ExpressionNode) {
+        self.expression = expr
+    }
+    
+    func switchChildren(node: Node) -> Node {
+        node.children.reverse() // Switch all children
+        return node
+    }
+}
