1249cbea43753b1cb2460a1a7ddb46d8b1086495
Arthur Guiot <arguiot@gmail.com>
2020-01-03 21:44:36 -0500

    Created NullNode

---
+++ Sources/Euler/Algebra/Parser/Node/NullNode.swift
@@ -0,0 +1,44 @@
+//
+//  NullNode.swift
+//  
+//
+//  Created by Arthur Guiot on 2020-01-03.
+//
+
+import Foundation
+
+/// This is a Node object that represents an empty Node
+public class NullNode: NSObject, Node {
+    /// Gives String representation of the node
+    public func toString() -> String {
+        return self.content
+    }
+    /// Gives Tex (String) representation of the node
+    public func toTex() -> String {
+        return self.content
+    }
+    
+    override public var description: String {
+        return "\(self.type)[ \(self.toString()) ]"
+    }
+    
+    public var content: String
+    
+    public var type: String = "NullNode"
+    
+    public var children = [Node]()
+    
+    /// Create a NullNode
+    public override init() {
+        self.content = ""
+    }
+    
+    /// Compiles NullNode to simpler node (useless here, but required by protocol)
+    public func compile() -> Node {
+        return self
+    }
+    /// Converts NullNode to BigNumber
+    public func evaluate(_ params: [String: BigNumber]) -> BigNumber {
+        return .zero
+    }
+}
