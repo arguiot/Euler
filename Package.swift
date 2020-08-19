515119c7f5874830d60ae8b4e0b0e7001c189d32
Arthur Guiot <arguiot@gmail.com>
2020-08-19 08:35:29 -0400

    Fixed issue with Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Euler",),
+        .target(name: "Euler"),
         .testTarget(
             name: "EulerTests",
             dependencies: ["Euler"]),
